c9283bffc6b4449e0a3042be6e31c247f14ea745
Jeremie Deray <artivis@users.noreply.github.com>
2017-11-05 19:57:31 +0100

    renamed FirstOf -> first_of

--- include/ts/first_of.h
+++ include/ts/first_of.h
@@ -1,19 +1,23 @@
 #ifndef _METACOL_TS_FIRST_OF_H_
 #define _METACOL_TS_FIRST_OF_H_
 
+namespace mco {
+
 template <class T, class Tuple>
-struct FirstOf { static constexpr int value = -1; };
+struct first_of { static constexpr int value = -1; };
 
 template <class T, template <class...> class C, class... Types>
-struct FirstOf<T, C<T, Types...>> {
+struct first_of<T, C<T, Types...>> {
     static constexpr int value = 0;
 };
 
 template <class T, class U, template <class...> class C, class... Types>
-struct FirstOf<T, C<U, Types...>> {
-    static_assert(FirstOf<T, C<Types...>>::value < sizeof...(Types)+1, "Type T not present in container");
+struct first_of<T, C<U, Types...>> {
+    static_assert(first_of<T, C<Types...>>::value < sizeof...(Types)+1, "Type T not present in container");
     static constexpr int value = 1 + FirstOf<T, C<Types...>>::value;
 };
+
+} /* namespace mco */
 
 #endif /* _METACOL_TS_FIRST_OF_H_ */
 
@@ -24,13 +28,13 @@ struct FirstOf<T, C<U, Types...>> {
 #include <iostream>
 #include <tuple>
 
-using foo_t = std::tuple<int, char*, int, long>;
+using foo_t = std::tuple<int, char, int, long>;
 
 int main() {
-    std::cout << "FirstOf<char, foo_t> = " << FirstOf<long, foo_t>::value << std::endl;
+    std::cout << "first_of<char, foo_t> = " << first_of<long, foo_t>::value << std::endl;
     
     // 'error: static assertion failed: Type T not present in tuple'
-    // std::cout << "FirstOf<char, foo_t> = " << FirstOf<float, foo_t>::value << std::endl;
+    // std::cout << "first_of<char, foo_t> = " << first_of<float, foo_t>::value << std::endl;
 }
 
 */
